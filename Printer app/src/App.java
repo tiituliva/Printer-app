d0ceadc9ce31f49d869ce5a8a56148dbf4a601c2
Tiia-Tuulia Vänttilä <amk1005402@student.hamk.fi>
2024-09-14 14:52:08 +0300

    Initial commit

---
+++ Printer app/src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hei olen Tulostin-ohjelma");
+        System.out.print("Ohjelman tekijä: ");
+        String tekijä;
+        double luku1, luku2;
+        luku1 = 5;
+        luku2 = 10;
+        double tulo;
+        tulo = luku1 * luku2;
+        double erotus;
+        erotus = luku1 - luku2;
+        double summa;
+        summa = luku1 + luku2;
+        double jako;
+        jako = luku1 / luku2;
+        tekijä = "Tiia Vänttilä";
+        System.out.println(tekijä);
+        System.out.println("Luku1-muuttujan arvo on " + luku1);
+        System.out.println("Luku2-muuttujan arvo on " + luku2);
+        System.out.println(luku1 + " * " + luku2 + " = " + tulo);
+        System.out.println("Valmis!");
+
+    }
+}
